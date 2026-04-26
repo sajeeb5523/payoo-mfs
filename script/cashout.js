0fdf8d1e45407737205438d09d962c6224d7752a
sajeeb5523 <sajeebaljabed1@gmail.com>
2026-04-26 22:08:16 +0600

    refactor: improve cashout logic

--- script/cashout.js
+++ script/cashout.js
@@ -1,10 +1,8 @@
-document.getElementById('cashout')
+document.getElementById('cashout_btn')
     .addEventListener('click', function (event) {
         event.preventDefault();
 
         const accountNumber = document.getElementById('cashout_account_number').value;
-
-        const bankName = document.getElementById('bank_name').value;
 
         const pin = document.getElementById('cashout_pin').value;
         const convertedPin = parseInt(pin);
@@ -15,33 +13,43 @@ document.getElementById('cashout')
         const mainBalance = document.getElementById('main_balance').innerText;
         const convertedMainBalance = parseFloat(mainBalance);
 
-        if (convertedPin === 1234) {
-            const sum = convertedMainBalance - convertedAmount;
-            document.getElementById('main_balance').innerText = sum;
+        if (convertedAmount > convertedMainBalance) {
+            alert('Insufficient balance');
+            return;
+        }
 
-            // cashout history show 
-            const container = document.getElementById('transaction_container');
-            const date = new Date();
-            const timestamp = date.toLocaleString('en-US', {
-                month: 'short',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit',
-                hour12: false
-            });
+        if (accountNumber.length === 11 && amount && pin) {
+            if (convertedPin === 1234) {
+                const sum = convertedMainBalance - convertedAmount;
+                document.getElementById('main_balance').innerText = sum;
 
-            const div = document.createElement('div');
-            div.classList.add('bg-white', 'mt-5', 'p-3', 'rounded');
-            div.innerHTML = `
-                <div class="text-sm text-slate-500">${timestamp}</div>
-                <div class="mt-2 text-slate-900">
-                    Cashed out <span class="text-rose-600 font-semibold">${- convertedAmount}</span> from account <span class="text-blue-600 font-semibold">${accountNumber}</span>
-                </div>
-            `;
-            container.appendChild(div);
+                // cashout history show 
+                const container = document.getElementById('transaction_container');
+                const date = new Date();
+                const timestamp = date.toLocaleString('en-US', {
+                    month: 'short',
+                    day: '2-digit',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    hour12: false
+                });
+
+                const div = document.createElement('div');
+                div.classList.add('bg-white', 'mt-5', 'p-3', 'rounded');
+                div.innerHTML = `
+                    <div class="text-sm text-slate-500">${timestamp}</div>
+                    <div class="mt-2 text-slate-900">
+                        Cashed out <span class="text-rose-600 font-semibold">${- convertedAmount}</span> from account <span class="text-blue-600 font-semibold">${accountNumber}</span>
+                    </div>
+                `;
+                container.appendChild(div);
+            }
+            else {
+                console.log('Enter valid pin');
+            }
         }
         else {
-            console.log('Enter valid pin');
+            alert('Enter valid account number, pin and some money');
         }
     })
