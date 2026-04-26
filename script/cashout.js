5a433f5f843497a8cd8c6f75cbf4ff55d5cc8ca3
sajeeb5523 <sajeebaljabed1@gmail.com>
2026-04-26 22:20:02 +0600

    refactor: again improve cashout logic

--- script/cashout.js
+++ script/cashout.js
@@ -18,38 +18,43 @@ document.getElementById('cashout_btn')
             return;
         }
 
-        if (accountNumber.length === 11 && amount && pin) {
-            if (convertedPin === 1234) {
-                const sum = convertedMainBalance - convertedAmount;
-                document.getElementById('main_balance').innerText = sum;
+        if (accountNumber.length === 11) {
+            if (convertedAmount > 0) {
+                if (convertedPin === 1234) {
+                    const sum = convertedMainBalance - convertedAmount;
+                    document.getElementById('main_balance').innerText = sum;
 
-                // cashout history show 
-                const container = document.getElementById('transaction_container');
-                const date = new Date();
-                const timestamp = date.toLocaleString('en-US', {
-                    month: 'short',
-                    day: '2-digit',
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    second: '2-digit',
-                    hour12: false
-                });
+                    // cashout history show 
+                    const container = document.getElementById('transaction_container');
+                    const date = new Date();
+                    const timestamp = date.toLocaleString('en-US', {
+                        month: 'short',
+                        day: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        second: '2-digit',
+                        hour12: false
+                    });
 
-                const div = document.createElement('div');
-                div.classList.add('bg-white', 'mt-5', 'p-3', 'rounded');
-                div.innerHTML = `
-                    <div class="text-sm text-slate-500">${timestamp}</div>
-                    <div class="mt-2 text-slate-900">
-                        Cashed out <span class="text-rose-600 font-semibold">${- convertedAmount}</span> from account <span class="text-blue-600 font-semibold">${accountNumber}</span>
-                    </div>
-                `;
-                container.appendChild(div);
+                    const div = document.createElement('div');
+                    div.classList.add('bg-white', 'mt-5', 'p-3', 'rounded');
+                    div.innerHTML = `
+                        <div class="text-sm text-slate-500">${timestamp}</div>
+                        <div class="mt-2 text-slate-900">
+                            Cashed out <span class="text-rose-600 font-semibold">${- convertedAmount}</span> from account <span class="text-blue-600 font-semibold">${accountNumber}</span>
+                        </div>
+                    `;
+                    container.appendChild(div);
+                }
+                else {
+                    alert('Enter valid pin');
+                }
             }
             else {
-                console.log('Enter valid pin');
+                alert('Add some money');
             }
         }
         else {
-            alert('Enter valid account number, pin and some money');
+            alert('Enter valid account number');
         }
     })
